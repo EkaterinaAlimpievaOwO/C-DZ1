ed23277216e3dfac605ec61b3309e9387f231efd
EkaterinaAlimpieva <AlimpievaOwO@yandex.ru>
2022-12-06 15:21:07 +0500

    feat: add solutions

--- Program.cs
+++ Program.cs
@@ -4,6 +4,19 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+
+// int number = int.Parse(Console.ReadLine());
+// int number2 = int.Parse(Console.ReadLine());
+// if (number > number2)
+// {
+//     Console.WriteLine("max = " + number);
+// }
+// else
+// {
+//     Console.WriteLine("max = " + number2);
+// }
+
+
 // ------------------------------------------------------------
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
@@ -11,6 +24,21 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+
+// int number = int.Parse(Console.ReadLine());
+// int number2 = int.Parse(Console.ReadLine());
+// int number3 = int.Parse(Console.ReadLine());
+// int max = number;
+
+// if (number2 > max) {
+//     max = number2;
+// }
+// if (number3 > max) {
+//     max = number3;
+// }
+// Console.WriteLine("Максимальное число = " + max);
+
 
 // ------------------------------------------------------------
 
@@ -20,11 +48,29 @@
 // -3 -> нет
 // 7 -> нет
 
+// int number = int.Parse(Console.ReadLine());
+// if(number % 2 == 0 ){
+//     Console.WriteLine("Да");
+// }
+// else{
+//     Console.WriteLine("Нет");
+// }
+
 // ------------------------------------------------------------
 
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+// int number = int.Parse(Console.ReadLine());
+// int i = 1;
+// while(i <= number){
+//     if(i % 2 == 0){
+//         Console.Write(i+" ");
+//     }
+//     i = i + 1;
+// }
+
 
 // ------------------------------------------------------------
